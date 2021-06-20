5a4abb072be94b3894d46563baa7e7d4dad39904
sinstranger <sinstranger@yandex.ru>
2021-06-20 09:59:39 +0300

    Lesson-8 Compleated

---
+++ Lesson-8/test.js
@@ -0,0 +1,26 @@
+if (!("a" in window)) {
+	var a = 1;
+}
+console.log(a);
+
+var b = function a(x) {
+	x && a(--x);
+};
+console.log(a);
+
+function a(x) {
+	return x * 2;
+}
+var a;
+console.log(a);
+
+function b(x, y, a) {
+	arguments[2] = 10;
+	console.log(a);
+}
+b(1, 2, 3);
+
+function a() {
+	console.log(this);
+}
+a.call(null);
